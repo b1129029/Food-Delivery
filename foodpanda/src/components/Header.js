d2f7b79a4c37c0c35ec69455ba0c862e6086b962
B1129029 <b1129029@cgu.edu.tw>
2024-11-26 23:35:51 +0800

    fix merge error

--- foodpanda/src/components/Header.js
+++ foodpanda/src/components/Header.js
@@ -20,18 +20,21 @@ import MenuIcon from "@mui/icons-material/Menu";
 import LanguageIcon from "@mui/icons-material/Language";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
+import LoginDialog from "../auth/LoginDialog"
 
 const GOOGLE_MAPS_API_KEY = "";
 
 const Header = () => {
   const [menuAnchorEl, setMenuAnchorEl] = React.useState(null);
+  const [openLoginDialog, setOpenLoginDialog] = React.useState(false);
   const [locationAnchorEl, setLocationAnchorEl] = React.useState(null);
   const [location, setLocation] = React.useState("");
   const [loadingLocation, setLoadingLocation] = React.useState(false);
 
   const handleMenuOpen = (event) => setMenuAnchorEl(event.currentTarget);
   const handleMenuClose = () => setMenuAnchorEl(null);
-
+  const handleLoginDialogOpen = () => setOpenLoginDialog(true);
+  const handleLoginDialogClose = () => setOpenLoginDialog(false);
   const handleLocationOpen = (event) => setLocationAnchorEl(event.currentTarget);
   const handleLocationClose = () => setLocationAnchorEl(null);
 
@@ -119,56 +122,6 @@ const Header = () => {
             </List>
           </Box>
         </Popover>
-        <Button color="inherit">登入</Button>
-        <Button variant="contained" color="secondary" sx={{ ml: 1 }}>
-          註冊
-        </Button>
-        <IconButton color="inherit" onClick={handleMenuOpen}>
-          <LanguageIcon />
-        </IconButton>
-        <Menu
-          anchorEl={menuAnchorEl}
-          open={Boolean(menuAnchorEl)}
-          onClose={handleMenuClose}
-        >
-          <MenuItem onClick={handleMenuClose}>繁體中文</MenuItem>
-          <MenuItem onClick={handleMenuClose}>English</MenuItem>
-        </Menu>
-        <IconButton color="inherit" sx={{ ml: 1 }}>
-          <ShoppingCartIcon />
-        </IconButton>
-        <Avatar sx={{ ml: 1 }}>F</Avatar>
-      </Toolbar>
-    </AppBar>
-  );
-};
-
-export default Header;
-
-import React, { useState } from "react";
-import { AppBar, Toolbar, Typography, Button, IconButton, Menu, MenuItem, Avatar } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
-import LanguageIcon from "@mui/icons-material/Language";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import LoginDialog from "../auth/LoginDialog"
-
-const Header = () => {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [openLoginDialog, setOpenLoginDialog] = useState(false);
-  const handleMenuOpen = (event) => setAnchorEl(event.currentTarget);
-  const handleMenuClose = () => setAnchorEl(null);
-  const handleLoginDialogOpen = () => setOpenLoginDialog(true);
-  const handleLoginDialogClose = () => setOpenLoginDialog(false);
-
-  return (
-    <AppBar position="static" color="inherit" sx={{ boxShadow: "none", borderBottom: "1px solid #e0e0e0" }}>
-      <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          foodpanda
-        </Typography>
         <Button color="inherit" onClick={handleLoginDialogOpen}>登入</Button>
         <Button variant="contained" color="secondary" sx={{ ml: 1 }} onClick={handleLoginDialogOpen}>
           註冊
@@ -177,8 +130,8 @@ const Header = () => {
           <LanguageIcon />
         </IconButton>
         <Menu
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
+          anchorEl={menuAnchorEl}
+          open={Boolean(menuAnchorEl)}
           onClose={handleMenuClose}
         >
           <MenuItem onClick={handleMenuClose}>繁體中文</MenuItem>
